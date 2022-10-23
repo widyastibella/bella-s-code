5fc2b9f33e2d86b6a4e70308a957598a121207a1
Widyasti Bella Kurnia <112605699+widyastibella@users.noreply.github.com>
2022-10-23 21:03:42 +0700

    Add files via upload

---
+++ Quick Maths 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float A;
+    cout<<"Silahkan masukkan angka:	";
+    cin>>A;
+    cout<< A <<" plus "<< A << " is "<< A+A << endl;
+    cout<< A+A<<" Minus one is " << A+A-1 << endl;
+    cout<< A+A-1<<" Multiple three is "<< (A+A-1)*3 << endl;
+    cout<< (A+A-1)*3<<" divide two is "<< (A+A-1)*3/2 << endl;
+    
+    return 0;
+}
+
